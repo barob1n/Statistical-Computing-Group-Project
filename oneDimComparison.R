712efd509d53acd3a614fe65b79e5541cb38285d
Spstolar <simonstolarczyk@gmail.com>
2015-11-25 08:54:36 -0600

    oneDimComparison
    Re add the file

---
+++ oneDimComparison.R
@@ -0,0 +1,19 @@
+#comparison of tests
+  #1D
+
+
+#sample amount
+m <- 50
+n <- 50
+
+#generate data
+X <- rnorm(m, 0, 1)
+Y <- rnorm(n, 1, 1)
+
+#run data through tests
+  #general tests
+
+
+#graph type 1 and type 2 errors
+#graph speed comparison
+#graph acceptance rate
